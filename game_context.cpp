575646222cb4a0d3835c7d08810e3eae6167d0b3
Collin Reeser <collin.reeser@gmail.com>
2016-10-04 23:51:46 -0400

    Restore drawing the beautiful borders on each game loop

--- game_context.cpp
+++ game_context.cpp
@@ -191,11 +191,6 @@ void GameContext::game_loop()
         )
     );
 
-    // This only needs to happen once, until window resizing is supported. Just
-    // drawing this once only works because we take advantage of our grid-based
-    // tile drawing and don't ever actually clear the screen
-    display->draw_borders();
-
     while (1)
     {
         for (auto it = creatures.begin(); it != creatures.end(); it++)
@@ -216,6 +211,8 @@ void GameContext::game_loop()
             );
 
             display->draw_event_messages(event_messages);
+
+            display->draw_borders();
 
             display->update_screen();
 
